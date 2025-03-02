9839898a5eca505df2fe3c910c4a532f6ec0642a
S12herifYehia <sherifyehia1111@gmail.com>
2025-03-02 15:01:44 +0200

    Services Worker Data

---
+++ js/app.js
@@ -0,0 +1,54 @@
+if("serviceWorker" in navigator){
+    // navigator // هو عباره عن السجل المدني للمتصفح بتاعي 
+    navigator.serviceWorker.register('../sw.js',{
+        scope:'/'
+    }).then((reg)=>{
+        console.log("File is register",reg)
+    }).catch((error)=>{
+        console.warn(error)
+    })
+
+  
+}
+
+// ثم هروح علي sw.js
+
+
+if("Notification" in window){
+    // متى يتم تشغيل هذا الجزء؟
+    // 🔹 يتم تشغيله فقط عند أول مرة يزور المستخدم الموقع أو إذا لم يسبق له إعطاء الإذن. 
+    Notification.requestPermission().then(per=>{
+        if(per == 'granted'){
+            console.log("Permission Is Granded")
+        }else{
+            console.log('Permission is Denied')
+        }
+    })
+}
+
+
+
+
+// إرسال إشعار إلى المستخدم بعد حصول الموقع على إذن الإشعارات.
+function sendNotification(){
+    // لتحقق مما إذا كان المتصفح يدعم Push API
+    if(window.PushManager){
+        // PushManager هو كائن مسؤول عن الإشعارات الفورية (Push Notifications).
+        navigator.serviceWorker.ready.then(reg=>{
+            // الانتظار حتى يصبح Service Worker جاهزًا
+            reg.showNotification('Eltorki Dev Hub',{
+                // showNotification(title, options) هو API خاص بـ Service Worker لإظهار إشعارات.
+                body:'Dev Hub Welcome ',
+                icon:"/img/icon/1.jpg",
+                requireInteraction: true, // ✅ سيبقي الإشعار ظاهرًا حتى يتم النقر عليه
+                persist: true, // يحاول إبقاء الإشعار في مركز الإشعارات (قد لا يكون مدعومًا في كل المتصفحات)
+                actions: [
+                    { action: "open", title: "افتح الموقع" },
+                    { action: "dismiss", title: "إغلاق" }
+                ]
+            })
+        })
+    }
+}
+
+// محتاج اشغل الرساله دي بالايفنت بوش 
---
+++ sw.js
@@ -0,0 +1,137 @@
+const version=6;
+const cacheName=`Portfolio-${version}`
+let assets=[
+    "/index.html",
+    "/css/style.css",
+    "/img/about.jpg",
+    "/img/box1.png",
+    "/img/layer2.jpg",
+    "/img/profile-1.jpeg",
+    "/img/profile-2.jpeg",
+    "/img/profile-3.jpeg",
+    "/img/profile-4.jpeg",
+    "/img/profile-5.jpeg",
+    "/img/teams1.jpg",
+    "/img/teams2.jpg",
+    "/img/teams3.jpg",
+    "/img/teams4.jpg",
+    "/css/font.css",
+    "/js/main.js",
+    "/js/jquery.js",
+    "/js/app.js",
+    "/manifest.json",
+]
+// لو الخط جايبه من بره احط اللينك عادي بدون اسلاش 
+
+self.addEventListener('install',(ev)=>{
+    console.log('Install')
+    // كده بقدر اخزن الملفات في الكاش 
+    ev.waitUntil(
+        caches.open(cacheName).then((cache)=>{
+
+            cache.addAll(assets).then().catch()
+    
+        }).catch((error)=>{
+            console.warn(error)
+        })
+    )
+
+    self.skipWaiting()
+  
+
+    // كده مش هيشتغل بدون ال wait 
+    // علشان بقوله خزنلي الملفات دي وخش علي الاكتفيتد افرض هو ملحقش يخزن الملفات دي يبقي كل الملفات مش هتتخزن 
+    // هعمل انو متخزنش الا لما تبقي جاهزه اتنقل 
+})
+
+
+self.addEventListener('activate',(ev)=>{
+    // بيجهز الملفات لما اعمل الابديت يمسح اي فيرجان قديم ويضيف الجديد 
+    // منه بيبدء يجهز اللي بعده 
+    // نخش علي ده بعد ماتخزن هو ايفنت بستخدمه علشان لما اعمل اي تحديث جديد بفريجن جديد امسح القديم 
+    ev.waitUntil(
+        
+        caches.keys().then((keys)=>{
+            return Promise.all(
+                            // يعني متنفزش الباقي الا لما تخلص ده
+                 keys.filter((key)=> key !== cacheName)
+                 .map((key)=>{
+                    return caches.delete(key)
+                 })
+            )
+            // هعمل الخاص بالاشعارات 
+        })
+
+        
+
+    )
+    
+})
+
+/*
+شرحهم المفروض لما بعمل ابديت بيروح من الانستول لاكتيفيت 
+ولو عملت الابديت روحت من الانستول لاكتفيت مش منطقي اللي يروحو لبعض بسرعه 
+في حاجه اسمها ويتنج علشان يستني وبعدين يروح لاكتفيتد
+
+
+
+*/
+// علشان يرجعلي البيانات من الكاش 
+self.addEventListener('fetch',(fetchdata)=>{
+// هنبدء مع ده ازاي نتعامل بيه 
+// وصل لمرحله اني اجيب البيانات من الكاش استورج 
+// لما اعمل ريفرش هيجيب كل الريكوسات اللي اقدر اعمل رجوع للبيانات 
+// الريكوستات اللي هشغلها اوف لاين 
+// console.log(fetchdata)
+
+
+// اول حاجه  الريسبونس اللي راجه من الكاش 
+
+fetchdata.respondWith(
+    // يعني هترجع اللي هيرجع مع الريسبونس ويز
+// هيبحث في الريكوستات 
+    caches.match(fetchdata.request).then(res=>{
+        return res || fetch(fetchdata.request) // في حاله لو الريسبونس دا نل هتجيبه من النت ورك 
+        // نروح نجرب نشغله اوف لاين هنلاقيه اشتعل
+        // واقدر اخزن صفحه اوف لاين بس دي اللي تتعرض اي ملف براحتي 
+    }) 
+
+)
+
+    
+})
+
+
+
+/*
+شرح الكاش اولا محتاج اعملها اسماء بفيرجان 
+
+في قلب الاوبن 
+add() لاضافه ملف واحد فقط
+addAll() لاضافه اكتر من ملف 
+match() لو عاوز اعمل سيرش حوالين ملف معين 
+delete() لملف معين او مجموعه ملفات 
+key() بجيب جميع الكي اللي عندي 
+put() لو عاوز اعمل ابديت للكاش 
+
+
+
+
+
+
+
+
+*/
+
+// !!!! PushNotification
+self.addEventListener('push',(NotificationEvent)=>{
+    // عايز اشغل الرساله 
+    NotificationEvent.waitUntil(
+        self.registration.showNotification()
+    )
+    
+})
+
+// وهروج اشوفه بيسمح بالاشعارات ام لاء 
+// ثم اروج اشوف النوتفيكشن موجوده في المتصفح هلاقي عرض الرساله النوتفكيشن لما بكلك علي الزر ولما اكلك عرض البدي والتايتل 
+// علشان يظهر في المتصفح بشغل الريكورد
